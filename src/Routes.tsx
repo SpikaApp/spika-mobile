d2c780c028153b6f94200e3dfd027afe1f2e43aa
xorgal <93151521+xorgal@users.noreply.github.com>
2023-01-07 14:15:42 +0300

    Add Send and ImportAccount Routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -5,7 +5,8 @@ import "react-native-gesture-handler";
 import { useSelector } from "react-redux";
 
 import { Toolbar } from "./components";
-import { Asset, ConfirmSeed, GenerateSeed, Home, InitAccount, Onboarding, Terms } from "./screens";
+import { Asset, ConfirmSeed, GenerateSeed, Home, InitAccount, Onboarding, Send, Terms } from "./screens";
+import { ImportAccount } from "./screens/Onboarding/ImportAccount";
 import type { RootState } from "./store";
 import { LightTheme } from "./theme";
 
@@ -14,11 +15,13 @@ export type RootStackScreenProps<T extends keyof RootStackParamList> = StackScre
 export type RootStackParamList = {
   Home: BaseScreenProps;
   Onboarding: BaseScreenProps;
-  Terms: BaseScreenProps;
+  Terms: TermsScreenProps;
   GenerateSeed: BaseScreenProps;
   ConfirmSeed: ConfirmSeedScreenProps;
-  InitAccount: ConfirmSeedScreenProps;
+  ImportAccount: BaseScreenProps;
+  InitAccount: InitAccountProps;
   Asset: AssetScreenProps;
+  Send: AssetScreenProps;
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -38,12 +41,14 @@ export const Routes = () => {
             <Stack.Screen name="Terms" component={Terms} />
             <Stack.Screen name="GenerateSeed" component={GenerateSeed} />
             <Stack.Screen name="ConfirmSeed" component={ConfirmSeed} />
+            <Stack.Screen name="ImportAccount" component={ImportAccount} />
             <Stack.Screen name="InitAccount" component={InitAccount} />
           </>
         )}
 
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Asset" component={Asset} />
+        <Stack.Screen name="Send" component={Send} />
       </Stack.Navigator>
     </NavigationContainer>
   );
