0037a8ae677915584c36980f4eae07a3a82ff9c1
xorgal <93151521+xorgal@users.noreply.github.com>
2023-01-07 14:00:48 +0300

    Change to Web3Client class

--- src/core/web3.ts
+++ src/core/web3.ts
@@ -1,7 +1,5 @@
 import * as aptos from "aptos";
 
-import { client } from "./client";
-
 export const aptosDerivePath = (i: number): string => {
   return `m/44'/637'/${i}'/0'/0'`;
 };
@@ -10,11 +8,45 @@ export const coinStore = (type: string): string => {
   return `0x1::coin::CoinStore<${type}>`;
 };
 
-export const getBalance = async (address: string, type: string): Promise<string | undefined> => {
-  const result: CoinStore = await client.getAccountResource(address, coinStore(type));
-  return result.data.coin?.value;
-};
+export class Web3Client {
+  readonly client: aptos.AptosClient;
 
-export const getAptosAccount = (index: number, mnemonic: string): aptos.AptosAccount => {
-  return aptos.AptosAccount.fromDerivePath(aptosDerivePath(index), mnemonic);
-};
+  constructor(nodeUrl: string) {
+    this.client = new aptos.AptosClient(nodeUrl);
+  }
+
+  getAccount = (index: number, mnemonic: string): aptos.AptosAccount => {
+    return aptos.AptosAccount.fromDerivePath(aptosDerivePath(index), mnemonic);
+  };
+
+  getBalance = async (address: string, type: string): Promise<string | undefined> => {
+    const result: CoinStore = await this.client.getAccountResource(address, coinStore(type));
+    return result.data.coin?.value;
+  };
+
+  simulateTransaction = async (
+    account: aptos.AptosAccount,
+    payload: aptos.TxnBuilderTypes.TransactionPayloadEntryFunction,
+    maxGasAmount: string,
+    gasUnitPrice: string
+  ): Promise<aptos.Types.UserTransaction | undefined> => {
+    const transaction = await this.client.generateRawTransaction(account.address(), payload, {
+      maxGasAmount: BigInt(maxGasAmount),
+      gasUnitPrice: BigInt(gasUnitPrice),
+    });
+    const bcsTxn = aptos.AptosClient.generateBCSSimulation(account, transaction);
+    return (await this.client.submitBCSSimulation(bcsTxn))[0];
+  };
+
+  submitTransaction = async (
+    account: aptos.AptosAccount,
+    payload: aptos.TxnBuilderTypes.TransactionPayloadEntryFunction,
+    maxGasAmount: string,
+    gasUnitPrice: string
+  ): Promise<aptos.Types.Transaction> => {
+    return await this.client.generateSignSubmitWaitForTransaction(account, payload, {
+      maxGasAmount: BigInt(maxGasAmount),
+      gasUnitPrice: BigInt(gasUnitPrice),
+    });
+  };
+}
